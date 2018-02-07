1537bee6f80e339cf373f19df9f4090f3bfc350e
pigunther <pigunther16@gmail.com>
2018-02-07 17:01:15 +0300

    change from localhost:8080 to localhost:1234

--- src/client/app/services/FlightLoad.service.ts
+++ src/client/app/services/FlightLoad.service.ts
@@ -10,8 +10,9 @@ export  class FlightLoadService {
   private JSONUrl = '/assets/flights.json';
   private result = '';
   //private BaseUrl = 'http://80.93.177.22:8080';
-  private BaseUrl = 'http://80.93.177.22:1234';
+  //private BaseUrl = 'http://80.93.177.22:1234';
   //private BaseUrl = 'http://localhost:5555';
+  private BaseUrl = 'http://localhost:1234';
 
   getTotalJson() : Promise<FlightModel[]>{
     return this.http.get('http://localhost:5555'+this.JSONUrl).toPromise().
@@ -21,9 +22,12 @@ export  class FlightLoadService {
   getJsonByStringQuery(flightQuery: FlightModel): Promise<FlightModel[]> {
 
     //this.result = `/api/search?cityFrom=${flightQuery.cityFrom}&cityTo=${flightQuery.cityTo}&date=${flightQuery.departureTime.valueOf()}&cost=${flightQuery.cost}`;
-    this.result = `/api/search?cityFrom=${flightQuery.airportFromObject.cityName}&cityTo=${flightQuery.airportToObject.cityName}&date=2017-12-12 1200:12:12&cost=${flightQuery.cost}`;
+    // this.result = `/api/search?cityFrom=${flightQuery.airportFrom.cityName}&cityTo=${flightQuery.airportTo.cityName}&date=1970-01-01 06:21:00&cost=${flightQuery.cost}`;
+    this.result = 'api/search?cityFrom=Белорецк&cityTo=Якутск&date=1970-01-01 00:00:00&cost=10000000';
 
-    return this.http.get(this.BaseUrl + this.result).toPromise().
+
+
+    return this.http.get('http://localhost:1234/api/search?cityFrom=Санкт-Петербург&cityTo=Томск&date=1970-01-01 00:00:00&cost=10000000').toPromise().
               then(response => response as FlightModel[]);
 
   }
