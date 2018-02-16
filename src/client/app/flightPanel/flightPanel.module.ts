22498e70a3fc7f1fddc9a199fba0d0e00c2c6dbe
pigunther <pigunther16@gmail.com>
2018-02-16 20:47:35 +0300

    annotation added

--- src/client/app/flightPanel/flightPanel.module.ts
+++ src/client/app/flightPanel/flightPanel.module.ts
@@ -1,20 +1,30 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
-import { FlightPanelComponent } from './flightPanel.component';
-import { FlightPanelRoutingModule } from './flightPanel-routing.module';
-import { SharedModule } from '../shared/shared.module';
-import { NameListService } from '../shared/name-list/name-list.service';
+import {FlightPanelComponent} from './flightPanel.component';
+import {FlightPanelRoutingModule} from './flightPanel-routing.module';
+import {SharedModule} from '../shared/shared.module';
+import {NameListService} from '../shared/name-list/name-list.service';
 import {
-  PanelModule, ToolbarModule, ButtonModule, TooltipModule, CalendarModule, AutoCompleteModule, SliderModule,
-  TabViewModule, RadioButtonModule
+  AutoCompleteModule,
+  ButtonModule,
+  CalendarModule,
+  DataListModule,
+  PanelModule,
+  RadioButtonModule,
+  SliderModule,
+  TabViewModule,
+  ToolbarModule,
+  TooltipModule
 } from 'primeng/primeng';
-import {FormsModule} from "@angular/forms";
-import {RouterModule} from "@angular/router";
-import {CommonModule} from "@angular/common";
-import {FlightOfferModule} from "../flightOffer/flightOffer.module";
-import {FlightLoadService} from "../services/FlightLoad.service";
-import {FlightMapModule} from "../flightMap/flightMap.module";
-import {FlightComplexOfferModule} from "../flightComplexOffer/flightComplexOffer.module";
-import {ListboxModule} from "primeng/listbox";
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FlightOfferModule} from '../flightOffer/flightOffer.module';
+import {FlightLoadService} from '../services/FlightLoad.service';
+import {FlightMapModule} from '../flightMap/flightMap.module';
+import {FlightComplexOfferModule} from '../flightComplexOffer/flightComplexOffer.module';
+
+// import { VirtualScrollModule } from 'angular2-virtual-scroll';
+
 
 @NgModule({
   imports: [FlightPanelRoutingModule,
@@ -33,7 +43,9 @@ import {ListboxModule} from "primeng/listbox";
             FlightMapModule,
             FlightComplexOfferModule,
             TabViewModule,
-            RadioButtonModule
+            RadioButtonModule,
+            DataListModule,
+            // VirtualScrollModule
             ],
   declarations: [FlightPanelComponent],
   exports: [FlightPanelComponent],
